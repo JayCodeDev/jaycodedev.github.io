f6273c21f3bbe70320836942841657beb3b98ce0
Austin Ramsey <61207236+ramseyaus12@users.noreply.github.com>
2021-07-30 13:39:51 -0400

    Many changes

--- assets/js/index-script.js
+++ assets/js/index-script.js
@@ -2,41 +2,41 @@
 // Step 1: Create reusable jQuery plugin
 // =====================================
 
-$.fancyConfirm = function( opts ) {
-  opts  = $.extend( true, {
-    title     : 'Are you sure?',
-    message   : '',
-    okButton  : 'OK',
-    noButton  : 'Cancel',
-    callback  : $.noop
-  }, opts || {} );
+$.fancyConfirm = function (opts) {
+  opts = $.extend(true, {
+    title: 'Are you sure?',
+    message: '',
+    okButton: 'OK',
+    noButton: 'Cancel',
+    callback: $.noop
+  }, opts || {});
 
   $.fancybox.open({
-    type : 'html',
-    src  :
-    '<div class="fc-content" style="text-align: center;">' +
-    '<h3>' + opts.title   + '</h3>' + '<br>' +
-    '<p class="tright">' +
-    '<a data-value="0" data-fancybox-close id="cancelBtn">' + opts.noButton + '</a>' +
-    '<button data-value="1" data-fancybox-close class="btn" id=""okBtn">' + opts.okButton + '</button>' +
-    '</p>' +
-    '</div>',
-    opts : {
-      animationDuration : 350,
-      animationEffect   : 'material',
-      modal : true,
-      baseTpl :
-      '<div class="fancybox-container fc-container" role="dialog" tabindex="-1">' +
-      '<div class="fancybox-bg"></div>' +
-      '<div class="fancybox-inner">' +
-      '<div class="fancybox-stage"></div>' +
-      '</div>' +
+    type: 'html',
+    src:
+      '<div class="fc-content" style="text-align: center;">' +
+      '<h3>' + opts.title + '</h3>' + '<br>' +
+      '<p class="tright">' +
+      '<a data-value="0" data-fancybox-close id="cancelBtn">' + opts.noButton + '</a>' +
+      '<button data-value="1" data-fancybox-close class="btn" id=""okBtn">' + opts.okButton + '</button>' +
+      '</p>' +
       '</div>',
-      afterClose : function( instance, current, e ) {
+    opts: {
+      animationDuration: 350,
+      animationEffect: 'material',
+      modal: true,
+      baseTpl:
+        '<div class="fancybox-container fc-container" role="dialog" tabindex="-1">' +
+        '<div class="fancybox-bg"></div>' +
+        '<div class="fancybox-inner">' +
+        '<div class="fancybox-stage"></div>' +
+        '</div>' +
+        '</div>',
+      afterClose: function (instance, current, e) {
         var button = e ? e.target || e.currentTarget : null;
-        var value  = button ? $(button).data('value') : 0;
+        var value = button ? $(button).data('value') : 0;
 
-        opts.callback( value );
+        opts.callback(value);
       }
     }
   });
@@ -45,19 +45,37 @@ $.fancyConfirm = function( opts ) {
 // Step 2: Start using it!
 // =======================
 
-$("#call_confirm").click(function() {
+$("#call_confirm").click(function () {
 
   // Open customized confirmation dialog window
   $.fancyConfirm({
-    title     : "Call (231) 709-3904",
-    okButton  : 'Continue',
-    noButton  : 'Cancel',
-    callback  : function (value) {
+    title: "Call (231) 709-3904",
+    okButton: 'Continue',
+    noButton: 'Cancel',
+    callback: function (value) {
       if (value) {
         console.log('continue');
         window.location = "tel:2317093904";
       } else {
         console.log('cancel');
+      }
+    }
+  });
+
+});
+
+$("#contact-sendmessage").click(function () {
+
+  // Open customized confirmation dialog window
+  $.fancyConfirm({
+    title: "Message Sent Successfully",
+    okButton: 'OK',
+    noButton: 'Cancel',
+    callback: function (value) {
+      if (value) {
+        console.log('OK');
+      } else {
+        console.log('Cancelled');
       }
     }
   });
